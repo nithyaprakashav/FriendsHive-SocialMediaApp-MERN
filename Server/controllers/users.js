cb0085a298363b44e7d81392972aee35af0f70dc
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-09 00:44:58 +0530

    Add addRemove Function

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -30,9 +30,30 @@ export const getUserFriends = async (req, res) => {
     }
 }
 
+//Update
+
 export const addRemoveFriends = async (req, res) => {
     try {
-        
+        const {_id , friendId} = req.params
+        const user = await User.findById(_id)
+        const friend = await User.findById(friendId)
+
+        if(user.friends.includes(friendId)){
+            user.friends =user.friends.filter((id)=> id!==friendId)
+            friend.friends = friend.friends.fiter((id) => id!=_id)
+        }else{
+            user.friends.push(friendId)
+            friend.friends.push(_id)
+        }
+        await user.save()
+        await friend.save()
+
+        const formattedFriends = friends.map(
+            ({_id , firstName , lastName, occupation , location , picturePath})=>{
+                return({_id , firstName , lastName , occupation , location , picturePath})
+            }
+        ) 
+        res.status(200).json(formattedFriends)       
     } catch (err) {
         res.status(404).json({mssg: err.message})
     }
