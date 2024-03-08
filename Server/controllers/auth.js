4c8d6e927549ad4c49a42610d678caec2e20e6fb
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-08 18:47:32 +0530

    Made few Changes

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -20,10 +20,40 @@ export const register = async (req , res) => {
         const salt = await bcrypt.genSalt();
         const passwordHash = await bcrypt.hash(password , salt)
 
-        const newUSer = new User({
-            
+        const newUser = new User({
+            firstName , 
+            lastName,
+            email , 
+            password: passwordHash,
+            picturepath,
+            friends,
+            location,
+            occupation,
+            viewedProfile:Math.floor(Math.random()*10000),
+            impressions:Math.floor(Math.random()*10000)
         })
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser)
     } catch (error) {
-        
+        res.status(500).json({error: error.message})
+    }
+}
+
+//Logging in
+
+export const login = async (req, res) => {
+    try {
+        const {email , password} = req.body
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({mssg:'User doesnot exist'})
+        }
+        const match = await bcrypt.compare(password,user.password)
+        if(!match){
+            return res.status(400).json({mssg: 'Incorrect Password'})
+        }
+        const token = jwt.sign({id: user._id} , process.env.SECRET)
+    } catch (err) {
+        res.status(500).json({error: err.message})
     }
 }
--- Server/server.js
+++ Server/server.js
@@ -9,6 +9,7 @@ import morgan from 'morgan'
 import path from 'path'
 import {fileURLToPath} from  'url'
 import {register} from './controllers/auth.js'
+import authRoutes from './Routes/auth.js'
 
 //Configurations
 const __filename = fileURLToPath(import.meta.url)
@@ -42,7 +43,7 @@ const upload = multer({storage})
 
 app.post('/auth/register' , upload.single('picture') , register)
 
-
+app.use('/auth', authRoutes)
 
 //Mongoose setup
 
