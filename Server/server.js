d97308d43f594268c011b5a0f95c7c640b69437b
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-09 12:47:48 +0530

    Add route

--- Server/server.js
+++ Server/server.js
@@ -9,8 +9,11 @@ import morgan from 'morgan'
 import path from 'path'
 import {fileURLToPath} from  'url'
 import {register} from './controllers/auth.js'
+import {createPost} from './controllers/posts.js'
 import authRoutes from './Routes/auth.js'
 import userRoutes from './Routes/userRoute.js'
+import postRoutes from './Routes/postRoute.js'
+import { verifyToken } from './middlewares/auth.js'
 
 //Configurations
 const __filename = fileURLToPath(import.meta.url)
@@ -43,10 +46,12 @@ const upload = multer({storage})
 //Routes with Files
 
 app.post('/auth/register' , upload.single('picture') , register)
+app.post('/posts' , verifyToken, upload.single('picture') , createPost)
 
 //Routes
 app.use('/auth', authRoutes)
 app.use('/users', userRoutes)
+app.use('/posts',postRoutes)
 
 //Mongoose setup
 
