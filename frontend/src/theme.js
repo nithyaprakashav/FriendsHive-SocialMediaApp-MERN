f6af29cbbc50d12a2d02f54beb5c32aa063a8ff7
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-10 02:46:51 +0530

    Few Chabges in color shades

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -39,7 +39,7 @@ export const colorTokens = {
                 primary:{
                     dark: colorTokens.primary[200],
                     main: colorTokens.primary[500],
-                    light: colorTokens.primary[800],
+                    light: colorTokens.primary[700],
                 },
                 neutral:{
                     dark: colorTokens.grey[100],
@@ -57,7 +57,7 @@ export const colorTokens = {
                 primary:{
                     dark: colorTokens.primary[700],
                     main: colorTokens.primary[500],
-                    light: colorTokens.primary[50]
+                    light: colorTokens.primary[300]
                 },
                 neutral:{
                     dark: colorTokens.grey[700],
