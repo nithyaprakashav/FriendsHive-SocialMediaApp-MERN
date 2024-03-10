762d312c7c15588275294c14966e3002cb0c8b0c
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-10 12:50:45 +0530

    Creating Home Page

--- frontend/src/scenes/loginPage/index.jsx
+++ frontend/src/scenes/loginPage/index.jsx
@@ -1,8 +1,35 @@
+import {Box , Typography , useTheme , useMediaQuery} from "@mui/material"
+
+
+
 const LoginPage = () => {
+    const theme = useTheme()
+    const isNonMobileScreens = useMediaQuery("min-width: 1000px")
+
+
     return ( 
-        <div className="login-page">
-            Login Page
-        </div>
+        <Box>
+        <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+           <Typography fontWeight="bold" fontSize="32px"
+            color="primary"
+            >
+                FriendsHive
+            </Typography>
+        </Box>
+        <Box 
+        width={isNonMobileScreens ? "50%" : "90%%"}
+        padding="2rem"
+        margin="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+        >
+            <Typography
+            fontWeight="500" variant="h5" sx={{mb:"1.5rem"}}
+            >
+
+            </Typography>
+        </Box>
+        </Box>
      );
 }
  
