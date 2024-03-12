2acb82d62fa1b73e303cb62299455c04a29b46ab
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-12 16:38:27 +0530

    Add token verification and redirection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import { themeSettings } from './theme.js';
 function App() {
   const mode = useSelector((state)=> state.mode)
   const theme = useMemo(() => createTheme(themeSettings(mode)),[mode])
+  const isAuthorized = Boolean(useSelector((state)=> state.token))
 
   return (
     <div className="app">
@@ -20,8 +21,8 @@ function App() {
         <CssBaseline/>
         <Routes>
           <Route path='/' element={<LoginPage/> }/>
-          <Route path='/home' element={<HomePage/> }/>
-          <Route path='/profile/:userId' element={<ProfilePage/> }/>
+          <Route path='/home' element={isAuthorized ? <HomePage/> : <Navigate to="/"/> }/>
+          <Route path='/profile/:userId' element={isAuthorized ? <ProfilePage/> : <Navigate to="/"/> }/>
         </Routes>
         </ThemeProvider>
       </BrowserRouter>
