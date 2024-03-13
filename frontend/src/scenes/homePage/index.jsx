14c3d562c398aa431182a05b94780c442c543da0
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-14 02:20:41 +0530

    Add homepage design

--- frontend/src/scenes/homePage/index.jsx
+++ frontend/src/scenes/homePage/index.jsx
@@ -1,10 +1,35 @@
-import{Box} from '@mui/material'
+import{Box, useMediaQuery} from '@mui/material'
+import { useSelector } from 'react-redux';
 import NavBar from 'scenes/navBar';
+import UserWidget from 'scenes/widgets/UserWidget';
 
 const HomePage = () => {
+
+    const isNonMobileScreens = useMediaQuery("(mid-width:100px)")
+    const { _id , picturePath} = useSelector((state)=> state.user)
+
     return (
         <Box>
            <NavBar/> 
+           <Box
+           width="100%"
+           p="2rem 6%"
+           display={isNonMobileScreens ? "flex" : "block"}
+           gap="0.5rem"
+           justifyContent="space-between"
+           >
+            <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+                <UserWidget  userId={_id} picturePath={picturePath} />
+            </Box>
+            <Box flexBasis={isNonMobileScreens ? "42%" : undefined} mt={isNonMobileScreens ? undefined : "2rem"}>
+
+            </Box>
+
+            {isNonMobileScreens && (
+                <Box flexBasis="26%"></Box>
+            )}
+
+           </Box>
         </Box>
     );
 }
