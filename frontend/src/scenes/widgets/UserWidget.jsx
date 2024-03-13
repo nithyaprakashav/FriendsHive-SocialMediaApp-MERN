250a1fb754a6bfe949d02a8c05f2dc61a51e0ca7
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-14 00:29:12 +0530

    Add 3rd 4th row

--- frontend/src/scenes/widgets/UserWidget.jsx
+++ frontend/src/scenes/widgets/UserWidget.jsx
@@ -82,8 +82,45 @@ const UserWidget = ({userId , picturePath}) => {
                 <Divider/>
 
                 {/* Second Row */}
+                <Box p="1rem 0">
+                    <Box display="flex"  alignItems="center" gap="1rem" mb="0.5rem">
+                    <LocationOnOutlined  fontSize="large" sx={{color:main}} />
+                    <Typography color={medium}>{location}</Typography>
+                    </Box>
+                    <Box display="flex"  alignItems="center" gap="1rem">
+                    <WorkOutlineOutlined  fontSize="large" sx={{color:main}} />
+                    <Typography color={medium}>{occupation}</Typography>
+                    </Box>
+                </Box>
 
+                {/* Third Row */}
+                <Box p="1rem 0">
+                    <FlexBetween mb="0.5rem">
+                        <Typography color={medium}> Who's viewed ypur profile</Typography>
+                        <Typography color={main} fontWeight="500">{viewedProfile}</Typography>
+                    </FlexBetween>
+                    <FlexBetween>
+                        <Typography color={medium}> Impressions of your Post</Typography>
+                        <Typography color={main} fontWeight="500">{impressions}</Typography>
+                    </FlexBetween>
+                </Box>
                 
+                {/* Fourth row */}
+                <Box p="1rem 0">
+                    <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">Social profiles</Typography>
+                    <FlexBetween gap="1rem" mb="0.5rem">
+                        <FlexBetween gap="1rem">
+                            <img src="../assets/twitter.png" alt="twitter"/>
+                            <Box>
+                                <Typography color={main} fontWeight="500">Twitter</Typography>
+                                <Typography color={medium}>Social Network</Typography>
+                            </Box>
+                        </FlexBetween>
+                        <EditOutlined sx={{color:main}}/>
+                    </FlexBetween>
+
+                    
+                </Box>
             </FlexBetween>
         </WidgetWrapper>
     )
