57ee88f37b9dd0d571c26312a45cfd0e8c6e4d01
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-14 17:19:28 +0530

    Fix typo

--- frontend/src/scenes/widgets/UserWidget.jsx
+++ frontend/src/scenes/widgets/UserWidget.jsx
@@ -95,7 +95,7 @@ const UserWidget = ({userId , picturePath}) => {
                 {/* Third Row */}
                 <Box p="1rem 0">
                     <FlexBetween mb="0.5rem">
-                        <Typography color={medium}> Who's viewed ypur profile</Typography>
+                        <Typography color={medium}> Who's viewed your profile</Typography>
                         <Typography color={main} fontWeight="500">{viewedProfile}</Typography>
                     </FlexBetween>
                     <FlexBetween>
