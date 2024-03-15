80d5c489c85a36e44cb56d9ab3fd0c5855eca50e
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-15 16:21:22 +0530

    Fix route

--- Server/Routes/postRoute.js
+++ Server/Routes/postRoute.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 //read
 router.get('/', verifyToken , getFeedPosts)
-router.get('/:userId', verifyToken , getUserPosts)
+router.get('/:userId/posts', verifyToken , getUserPosts)
 
 //Update
 router.patch('/:id/like', verifyToken , likePost)
